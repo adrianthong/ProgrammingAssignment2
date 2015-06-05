a00f09af52fb477e0839fdf6943196db78ec3c10
adrianthong <adrian_thong@yahoo.com>
2015-06-05 18:18:52 +0800

    Change code for solve fun

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,27 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        
+        #clear inverse matrix and store matrix
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        
+        #return matrix        
+        get <- function() x
+        
+        #store inverse matrix
+        setsolve <- function(solve) m <<- solve
+        
+        #return inverse matrix
+        getsolve <- function() m
+        
+        #list all functions
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
@@ -12,4 +32,20 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        
+        if (!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        } else {
+                message("calculating inverse matrix")
+        }
+        
+        data <- x$get()
+        x$set(data)
+        
+        m <- solve(data, ...)
+        x$setsolve(m)
+        
+        m
 }
